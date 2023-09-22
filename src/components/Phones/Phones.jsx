984602b1cf4f5830e71433038b79c8bc802447e4
Rakib <khandokerrakib.hasan@bracits.com>
2023-09-22 11:48:42 +0600

    use awesome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-
+import { BarChart, Bar, XAxis, YAxis, Tooltip} from 'recharts';
+import { Audio, Grid } from  'react-loader-spinner'
 
 const Phones = () => {
 
-    const [phones, setPhones] = useState([])
+    const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -22,12 +23,35 @@ const Phones = () => {
                 }
                 return obj;
             })
-            setPhones(phoneTempData)
+            setPhones(phoneTempData);
+            setLoading(false);
         })
     },[])
 
     return (
         <div>
+            {loading && <div>
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color='green'
+                    ariaLabel='three-dots-loading'
+                    wrapperStyle
+                    wrapperClass
+                />
+
+                <Grid
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="grid-loading"
+                    radius="12.5"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                />
+            </div>}
             <BarChart width={800} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
                 <XAxis dataKey="name"></XAxis>
